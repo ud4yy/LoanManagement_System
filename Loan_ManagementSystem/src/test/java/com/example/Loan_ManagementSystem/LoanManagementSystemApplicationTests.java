fef214dcbd47e5d53f9ec18dc4079824ad99fade
ud4yy <ud4yworks@gmail.com>
2024-08-29 15:42:31 +0530

    init

---
+++ Loan_ManagementSystem/src/test/java/com/example/Loan_ManagementSystem/LoanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loan_ManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
